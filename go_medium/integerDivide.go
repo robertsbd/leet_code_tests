9fa5699b5f4e3df69870a2694b4e56d7f128e2bd
Benjamin Roberts <roberts.b.d@pm.me>
2024-04-07 21:47:03 +0100

    Update integerDivide.go

--- go_medium/integerDivide.go
+++ go_medium/integerDivide.go
@@ -1,9 +1,25 @@
 // This is a brute force implementation, and I could implement a long division algorithm, but I can't be bothered
+// Integer divide function implemented with out using *, /, % or any Math functions that may make use of these
 
 func checkIfLarge(n int) int {
 
     if n > 2147483647 {return 2147483647}
     if n < -2147483648 {return -2147483648}
+    return n
+}
+
+func getSign(x, y int) int {
+
+    if (x > 0 && y < 0) || (x < 0 && y > 0) {
+        return - 1
+    }
+    return 1
+}
+
+func setSign(n, sign int) int {
+    if sign == - 1 {
+        return n - n - n
+    }
     return n
 }
 
@@ -15,25 +31,24 @@ func divide(dividend int, divisor int) int {
     var divi int = divisor
 
     if rem < 0 {
-        rem = rem * -1
-        sign = sign * -1
+        rem = rem - rem - rem
+        sign = -1
     }
 
     if divi < 0 {
-        divi = divi * -1
-        sign = sign * -1
+        divi = divi - divi - divi
+        sign = getSign(-1, sign)
     }
 
-    if divi > rem {return 0}
-
-    if divi == rem {return 1 * sign}
-
-    if divi == 1 {return checkIfLarge(rem * sign)}
-
-    for rem >= divi {
-        rem = rem - divi
-        n++
+    if divi == 1 {n = rem
+    } else if divi > rem {n = 0
+    } else if divi == rem {n = 1
+    } else {
+        for rem >= divi {
+            rem -= divi
+            n++
+        }
     }
 
-    return checkIfLarge(n * sign)
+    return checkIfLarge(setSign(n, sign))
 }
